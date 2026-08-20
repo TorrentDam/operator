771db10840596fb1c94fe59221c251359eada5a8
Vitaly Lavrov <lavrovvv@gmail.com>
2026-08-20 10:11:47 +0200

    Remove pod replace from reconcile; pods are immutable once created

--- operator.scala
+++ operator.scala
@@ -229,12 +229,8 @@ class Operator(client: KClient[IO])(using logger: Logger[IO]):
     val currentPods = podAPI.list().send(client).await
     val currentPod = currentPods.items.find(pod => pod.metadata.exists(_.name == Some(podName)))
     currentPod match
-      case Some(current) =>
-        if current.spec == desired.spec then
-          Logger[IO].debug(s"Pod $podName spec unchanged for torrent $crName, skipping replace").await
-        else
-          podAPI.replace(podName, desired).send(client).await
-          Logger[IO].info(s"Replaced pod $podName for torrent $crName").await
+      case Some(_) =>
+        Logger[IO].debug(s"Pod $podName already exists for torrent $crName").await
       case None =>
         podAPI.create(desired).send(client).await
         Logger[IO].info(s"Created pod $podName for torrent $crName").await
